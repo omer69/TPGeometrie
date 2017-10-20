cf4c3b1dccf3c2e0470c220052410ff138221f02
csnd2349 <csnd2349@S8707.labosio.fr>
2017-10-20 16:54:37 +0200

    tp geometrie debut

---
+++ src/tpgeometrie/Polygone.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgeometrie;
+
+/**
+ *
+ * @author csnd2349
+ */
+public class Polygone {
+    private int numero;
+    private String nom;
+}
